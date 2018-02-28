7fc56f0878cd06848e4f9fb15901abd04ad7c5b2
yangb8 <bo.yang@emc.com>
2018-02-28 11:13:45 -0500

    implement WritableComparable interface for EventKey class (#13)

--- src/test/java/io/pravega/connectors/hadoop/EventKeyTest.java
+++ src/test/java/io/pravega/connectors/hadoop/EventKeyTest.java
@@ -58,4 +58,26 @@ public class EventKeyTest {
         Assert.assertEquals(0, key.getSplit().getSegment().compareTo(inKey.getSplit().getSegment()));
         Assert.assertEquals(key.getOffset(), inKey.getOffset());
     }
+
+    @Test
+    public void testEventKeyComparable() throws IOException {
+        Segment segment1 = new Segment(TEST_SCOPE, TEST_STREAM, 10);
+        PravegaInputSplit split1 = new PravegaInputSplit(segment1, 1, 100);
+        EventKey key1 = new EventKey(split, 5L);
+
+        for (int seg = 9; seg <= 11; seg++) {
+            for (int end = 99; end <= 101; end++) {
+                for (long off = 4L; off <= 6L; off++) {
+                    Segment segment2 = new Segment(TEST_SCOPE, TEST_STREAM, seg);
+                    PravegaInputSplit split2 = new PravegaInputSplit(segment2, 1, end);
+                    EventKey key2 = new EventKey(split2, off);
+                    if (split1.compareTo(split2) == 0) {
+                        Assert.assertTrue(key1.compareTo(key2) == Long.compare(5L, off));
+                    } else {
+                        Assert.assertTrue(key1.compareTo(key2) == split1.compareTo(split2));
+                    }
+                }
+            }
+        }
+    }
 }
