6a95b8746c32bb420015ec5a0412ff0eae4a2dc2
Brian Zhou <brian.zhou@emc.com>
2020-12-09 23:01:13 +0800

    [issue-143] Update Java 11 and Hadoop 3.3 and latest Pravega (#144)
    Signed-off-by: Brian Zhou <b.zhou@dell.com>

--- gradlew.bat
+++ gradlew.bat
@@ -1,19 +1,19 @@
+@rem
+@rem Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+@rem
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
+
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
 @rem
 @rem ##########################################################################
-
-@rem
-@rem  Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
-@rem
-@rem  Licensed under the Apache License, Version 2.0 (the "License");
-@rem  you may not use this file except in compliance with the License.
-@rem  You may obtain a copy of the License at
-@rem
-@rem      http://www.apache.org/licenses/LICENSE-2.0
-@rem
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
@@ -22,6 +22,9 @@ set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
+
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
@@ -74,6 +77,7 @@ set CMD_LINE_ARGS=%*
 @rem Setup the command line
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
